a64e9c45b09c988f61fbb56c03a24cf8aa8af67a
simon <simondjv2016@gmail.com>
2025-02-04 21:35:42 -0500

    User authentication

---
+++ server/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HealthCheck(APIView):
+    def get(self, request):
+        return Response({"Running": True})
